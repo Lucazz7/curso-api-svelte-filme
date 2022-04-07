758b16baa968f3c704b13ea570b7cc659ed8cd60
Lucas Carlos <lucascarls002@gmail.com>
2022-04-07 17:25:54 -0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<!-- svelte-ignore a11y-missing-content -->
+<a href="/about"> vamos para o svelte</a>
+
+<style>
+    p{
+        font-size: 2rem;
+    }
+</style>
